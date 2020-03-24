638b982d0cc266864950cffed42cf4c5ab111ee5
Apple <apple@MacBook-Pro-Apple.local>
2020-03-24 18:11:37 +0200

    Making dictionary of files

---
+++ Dictionary/src/main.py
@@ -0,0 +1,34 @@
+import re
+import sys
+import json
+import pickle
+
+all_words = 0
+unique_words = 0
+dictionary = {}
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
+    with open(sys.argv[i]) as f_reader:
+        data = f_reader.read().lower()
+        pattern = re.compile("(?<=[> \n\t])*([a-z0-9][a-z0-9']*-?[a-z0-9][a-z0-9']+)(?=[< \n\t])*")
+        find = re.findall(pattern, data)
+        all_words += len(find)
+        for word in find:
+            try:
+                dictionary[word]
+            except KeyError:
+                dictionary[word] = 0
+                unique_words += 1
+
+dictionary = sorted(dictionary.keys())
+
+f_writer = open('../result/output.json', 'w')
+json.dump(dictionary, f_writer)
+bf_writer = open('../result/output.pickle', 'bw')
+pickle.dump(dictionary, bf_writer)
+
+print('Quantity of words: ' + str(all_words))
+print('Quantity of unique words: ' + str(unique_words))
+
+f_writer.close()
+bf_writer.close()
